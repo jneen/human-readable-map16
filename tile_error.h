74cef32e59e87db3f953ce632f0f1491093a17d1
underway <underwayu@gmail.com>
2022-02-16 14:45:15 +0100

    Add method for getting detailed error string to TileError class
    `get_detailed_error_message` provides a nice and detailed error message for users.

--- tile_error.h
+++ tile_error.h
@@ -4,8 +4,11 @@
 #include <filesystem>
 namespace fs = std::filesystem;
 
+#include <sstream>
+
 #include "data_error.h"
 #include "tile_format.h"
+#include "human_readable_map16.h"
 
 class TileError : public DataError {
 	private:
@@ -25,5 +28,40 @@ class TileError : public DataError {
 
 		unsigned int get_tile_number() {
 			return expected_tile_number;
+		}
+
+		std::string get_detailed_error_string() {
+			std::ostringstream s;
+			s << "Error in file \"" << get_file_path().string() << "\" on line " << get_line_number() << " at character " << get_char_index() << ":" << std::endl <<
+				"    \"" << get_line() << '\"' << std::endl;
+			for (int i = 0; i != get_char_index() + 4; i++) {
+				s << " ";
+			}
+			s << "^" << std::endl;
+			for (int i = 0; i != get_char_index() + 4; i++) {
+				s << " ";
+			}
+			s << "|" << std::endl;
+			for (int i = 0; i != get_char_index() + 4; i++) {
+				s << " ";
+			}
+			s << get_message() << std::endl;
+			s << "Expected format example (";
+
+			switch (expected_tile_format) {
+				case TileFormat::FULL:
+					s << "acts like settings & 8x8 tiles):" << std::endl << "    \"" << HumanReadableMap16::STANDARD_FORMAT_EXAMPLE;
+					break;
+				case TileFormat::ACTS_LIKE_ONLY:
+					s << "acts like settings only):" << std::endl << "    \"" << HumanReadableMap16::NO_TILES_EXAMPLE;
+					break;
+				case TileFormat::TILES_ONLY:
+					s << "8x8 tiles only):" << std::endl << "    \"" << HumanReadableMap16::NO_ACTS_FORMAT_EXAMPLE;
+					break;
+			}
+
+			s << '\"';
+
+			return s.str();
 		}
 };
